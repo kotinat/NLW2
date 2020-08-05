89fa07cd2b37be49bf10efa4529bd113cca1d000
Natalia Koti <natalia.meneghetti@paygo.com.br>
2020-08-04 22:55:32 -0300

    feat(teacher-list): add styles

--- my-app/src/pages/TeacherList/index.tsx
+++ my-app/src/pages/TeacherList/index.tsx
@@ -2,20 +2,19 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import backIcon from "../../assets/images/icons/back.svg";
 import logoImg from "../../assets/images/logo.svg";
+import "./styles.css";
 
 const TeacherList = () => {
   return (
     <div id="page-teacher-list" className="container">
-      <header id="page-header">
-        <div id="top-bar-container">
+      <header className="page-header">
+        <div className="top-bar-container">
           <Link to="/">
             <img src={backIcon} alt="Voltar" />
           </Link>
-          <Link to="/">
-            <img src={logoImg} alt="Proffy" />
-          </Link>
+          <img src={logoImg} alt="Proffy" />
         </div>
-        <div id="header-content">
+        <div className="header-content">
           <strong>Estes são os proffys disponíveis</strong>
         </div>
       </header>
